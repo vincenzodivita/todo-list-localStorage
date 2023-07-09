0d86db78bfd15d429c0d307ee290f6a6ad97f1d6
vincenzodivita <divitavincenzo@icloud.com>
2023-07-09 16:17:23 +0200

    Change add fn for printing on HTML

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,16 @@
 const newTask =document.querySelector('input');
 const list = document.querySelector('ul');
 
-// Function that print localStorage on console
+// Function that print localStorage on HTML as <li>
 function add() {
-    localStorage.setItem(localStorage.length, newTask.value)
-    newTask.value =''
-    console.log(localStorage)
+    localStorage.setItem(localStorage.length, newTask.value);
+    newTask.value = '';
+
+    list.innerHTML = ''
+    for (let i = localStorage.length - 1; i >= 0; i--) {
+        list.innerHTML += `<li>${localStorage.getItem(i)}</li>`;
+    }
 }
 
 // Temporary localStorage clear on page load
-localStorage.clear()
+localStorage.clear();
