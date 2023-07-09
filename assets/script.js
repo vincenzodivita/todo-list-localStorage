614cb80a915b872035796b767c97b594ae8e108a
vincenzodivita <divitavincenzo@icloud.com>
2023-07-09 16:21:15 +0200

    Divide add fn into two fn and let load fn runs on page load

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,22 @@
 const newTask =document.querySelector('input');
 const list = document.querySelector('ul');
 
-// Function that print localStorage on HTML as <li>
-function add() {
-    localStorage.setItem(localStorage.length, newTask.value);
-    newTask.value = '';
-
+function load() {
     list.innerHTML = ''
     for (let i = localStorage.length - 1; i >= 0; i--) {
         list.innerHTML += `<li>${localStorage.getItem(i)}</li>`;
     }
+};
+
+// Function that print localStorage on HTML as <li>
+function add() {
+    localStorage.setItem(localStorage.length, newTask.value);
+    newTask.value = '';
+    load(); 
 }
 
 // Temporary localStorage clear on page load
-localStorage.clear();
+// localStorage.clear();
+
+// Run load fn when open page
+load()
